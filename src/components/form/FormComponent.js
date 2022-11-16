2ec085d069793171f485aa40d6541d2307ee601d
eyupGuc <meydanguresi@gmail.com>
2022-11-16 04:53:51 +0300

    move

--- src/components/form/FormComponent.js
+++ src/components/form/FormComponent.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import React from "react";
 
 const FormComponent = () => {
   return (
-    <div>FormComponent</div>
-  )
-}
+    <Container maxWidth="md">
+      <Grid
+        textAlign="center"
+        verticalAlign="middle"
+        direction="column"
+        style={{ width: "300" }}
+      >
+        <p className="contact-header">
+          <div>
+            <a
+              href="#"
+              className="design"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <code>{"<eGuc/>"}</code>
+            </a>
+          </div>
+          <span className="design header">design</span>
+        </p>
 
-export default FormComponent
+        <h2 className="contact-header">Add Contact</h2>
+        <Box style={{ backgroundColor: "White", padding: "20px" }}></Box>
+      </Grid>
+    </Container>
+  );
+};
+
+export default FormComponent;
