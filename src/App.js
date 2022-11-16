bde1aa3376d86eddf6ae65b9ba982cca9c2b96b9
eyupGuc <meydanguresi@gmail.com>
2022-11-16 04:34:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import FormComponent from "./components/form/FormComponent";
+import Contacts from "./components/contacts/Contacts";
+
+const App=()=>{
+
+  return (<div>
+<FormComponent/>
+<Contacts/>
+
+  </div>)
+}
+
+export default App;
